180458075450c9d69d33a21c67987762a2a65f02
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-05-17 14:38:12 +0100

    Try and refactor jack process creation

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,19 @@ extern crate jack;
 use jack::prelude as j;
 use std::io;
 
+fn makeProcess(in_a: &j::Port<j::AudioInSpec>, in_b: &j::Port<j::AudioInSpec>, out_a: &mut j::Port<j::AudioOutSpec>, out_b: &mut j::Port<j::AudioOutSpec>) {
+    let process_callback = move |_: &j::Client, ps: &j::ProcessScope| -> j::JackControl {
+        let mut out_a_p = j::AudioOutPort::new(&mut out_a, ps);
+        let mut out_b_p = j::AudioOutPort::new(&mut out_b, ps);
+        let in_a_p = j::AudioInPort::new(&in_a, ps);
+        let in_b_p = j::AudioInPort::new(&in_b, ps);
+        out_a_p.clone_from_slice(&in_a_p);
+        out_b_p.clone_from_slice(&in_b_p);
+        j::JackControl::Continue
+    };
+    j::ClosureProcessHandler::new(process_callback);
+}
+
 fn main() {
     // Create client
     let (client, _status) = j::Client::new("rust_jack_simple", j::client_options::NO_START_SERVER)
@@ -13,16 +26,7 @@ fn main() {
     let in_b = client.register_port("rust_in_r", j::AudioInSpec::default()).unwrap();
     let mut out_a = client.register_port("rust_out_l", j::AudioOutSpec::default()).unwrap();
     let mut out_b = client.register_port("rust_out_r", j::AudioOutSpec::default()).unwrap();
-    let process_callback = move |_: &j::Client, ps: &j::ProcessScope| -> j::JackControl {
-        let mut out_a_p = j::AudioOutPort::new(&mut out_a, ps);
-        let mut out_b_p = j::AudioOutPort::new(&mut out_b, ps);
-        let in_a_p = j::AudioInPort::new(&in_a, ps);
-        let in_b_p = j::AudioInPort::new(&in_b, ps);
-        out_a_p.clone_from_slice(&in_a_p);
-        out_b_p.clone_from_slice(&in_b_p);
-        j::JackControl::Continue
-    };
-    let process = j::ClosureProcessHandler::new(process_callback);
+    let process = makeProcess(&in_a, &in_b, &mut out_a, &mut out_b);
 
     // Activate the client, which starts the processing.
     let active_client = j::AsyncClient::new(client, (), process).unwrap();
