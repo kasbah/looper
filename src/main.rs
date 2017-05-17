127a9bfe233901418a50a77fab24f5beba3977d4
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-05-17 19:50:15 +0100

    Make a process struct

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,23 @@ extern crate jack;
 use jack::prelude as j;
 use std::io;
 
-fn makeProcess(in_a: &j::Port<j::AudioInSpec>, in_b: &j::Port<j::AudioInSpec>, out_a: &mut j::Port<j::AudioOutSpec>, out_b: &mut j::Port<j::AudioOutSpec>) {
-    let process_callback = move |_: &j::Client, ps: &j::ProcessScope| -> j::JackControl {
-        let mut out_a_p = j::AudioOutPort::new(&mut out_a, ps);
-        let mut out_b_p = j::AudioOutPort::new(&mut out_b, ps);
-        let in_a_p = j::AudioInPort::new(&in_a, ps);
-        let in_b_p = j::AudioInPort::new(&in_b, ps);
+struct Process {
+    in_a: j::Port<j::AudioInSpec>,
+    in_b: j::Port<j::AudioInSpec>,
+    out_a: j::Port<j::AudioOutSpec>,
+    out_b: j::Port<j::AudioOutSpec>,
+}
+
+impl Process {
+    fn callback(& mut self, _: & j::Client, ps: & j::ProcessScope) -> j::JackControl {
+        let mut out_a_p = j::AudioOutPort::new(&mut self.out_a, ps);
+        let mut out_b_p = j::AudioOutPort::new(&mut self.out_b, ps);
+        let in_a_p = j::AudioInPort::new(&self.in_a, ps);
+        let in_b_p = j::AudioInPort::new(&self.in_b, ps);
         out_a_p.clone_from_slice(&in_a_p);
         out_b_p.clone_from_slice(&in_b_p);
         j::JackControl::Continue
-    };
-    j::ClosureProcessHandler::new(process_callback);
+    }
 }
 
 fn main() {
@@ -22,14 +28,16 @@ fn main() {
 
     // Register ports. They will be used in a callback that will be
     // called when new data is available.
-    let in_a = client.register_port("rust_in_l", j::AudioInSpec::default()).unwrap();
-    let in_b = client.register_port("rust_in_r", j::AudioInSpec::default()).unwrap();
-    let mut out_a = client.register_port("rust_out_l", j::AudioOutSpec::default()).unwrap();
-    let mut out_b = client.register_port("rust_out_r", j::AudioOutSpec::default()).unwrap();
-    let process = makeProcess(&in_a, &in_b, &mut out_a, &mut out_b);
+    let mut process = Process {
+        in_a  : client.register_port("rust_in_l", j::AudioInSpec::default()).unwrap(),
+        in_b  : client.register_port("rust_in_r", j::AudioInSpec::default()).unwrap(),
+        out_a : client.register_port("rust_out_l", j::AudioOutSpec::default()).unwrap(),
+        out_b : client.register_port("rust_out_r", j::AudioOutSpec::default()).unwrap(),
+    };
+    let p = j::ClosureProcessHandler::new(move |client: &j::Client, ps: &j::ProcessScope| process.callback(client, ps));
 
     // Activate the client, which starts the processing.
-    let active_client = j::AsyncClient::new(client, (), process).unwrap();
+    let active_client = j::AsyncClient::new(client, (), p).unwrap();
 
     // Wait for user input to quit
     println!("Press enter/return to quit...");
