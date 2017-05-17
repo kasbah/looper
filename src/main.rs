1023872b1a383f471d095c57019b8138d7b77ef0
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-05-17 14:37:12 +0100

    Use jack example as main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate jack;
+use jack::prelude as j;
+use std::io;
+
 fn main() {
-    println!("hey");
+    // Create client
+    let (client, _status) = j::Client::new("rust_jack_simple", j::client_options::NO_START_SERVER)
+        .unwrap();
+
+    // Register ports. They will be used in a callback that will be
+    // called when new data is available.
+    let in_a = client.register_port("rust_in_l", j::AudioInSpec::default()).unwrap();
+    let in_b = client.register_port("rust_in_r", j::AudioInSpec::default()).unwrap();
+    let mut out_a = client.register_port("rust_out_l", j::AudioOutSpec::default()).unwrap();
+    let mut out_b = client.register_port("rust_out_r", j::AudioOutSpec::default()).unwrap();
+    let process_callback = move |_: &j::Client, ps: &j::ProcessScope| -> j::JackControl {
+        let mut out_a_p = j::AudioOutPort::new(&mut out_a, ps);
+        let mut out_b_p = j::AudioOutPort::new(&mut out_b, ps);
+        let in_a_p = j::AudioInPort::new(&in_a, ps);
+        let in_b_p = j::AudioInPort::new(&in_b, ps);
+        out_a_p.clone_from_slice(&in_a_p);
+        out_b_p.clone_from_slice(&in_b_p);
+        j::JackControl::Continue
+    };
+    let process = j::ClosureProcessHandler::new(process_callback);
+
+    // Activate the client, which starts the processing.
+    let active_client = j::AsyncClient::new(client, (), process).unwrap();
+
+    // Wait for user input to quit
+    println!("Press enter/return to quit...");
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input).ok();
+
+    active_client.deactivate().unwrap();
 }
