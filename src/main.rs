a10931cef6ed86e1cec237eebe8645385f2a9d42
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-05-18 17:22:16 +0100

    Use channel for setting thru

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,16 @@ use jack::prelude as j;
 
 use std::io::{self, Read};
 
+use std::sync::mpsc::sync_channel;
+
+// Create a simple streaming channel
+
 mod looper;
 use looper::*;
+
+enum Messages {
+    SetThru(f32),
+}
 
 fn main() {
     // Create client
@@ -19,6 +27,7 @@ fn main() {
     let mut out_b  = client.register_port("rust_out_r", j::AudioOutSpec::default()).unwrap();
     let midi_in    = client.register_port("rust_midi_in", j::MidiInSpec::default()).unwrap();
     let mut looper = Looper::new();
+    let (tx, rx) = sync_channel::<Messages>(0);
     let process_callback = move |_: &j::Client, ps: &j::ProcessScope| -> j::JackControl {
         let mut out_a_p = j::AudioOutPort::new(&mut out_a, ps);
         let mut out_b_p = j::AudioOutPort::new(&mut out_b, ps);
@@ -27,6 +36,13 @@ fn main() {
         let midi_in_p   = j::MidiInPort::new(&midi_in, ps);
         for e in midi_in_p.iter() {
             println!("{:?}", e);
+        }
+        let msg = rx.try_recv();
+        match msg {
+            Ok(Messages::SetThru(value)) => {
+                looper.thru = value;
+            },
+            _ => {}
         }
         looper.run(ps.n_frames(), &in_a_p, &in_b_p, &mut out_a_p, &mut out_b_p);
         j::JackControl::Continue
@@ -43,20 +59,24 @@ fn main() {
         io::stdin().read_line(&mut user_input).ok();
         match user_input.as_ref() {
             "q\n" => {
-                println!("exiting");
                 break;
             },
             "p\n" => {
                 println!("pausing");
-                looper.state = LooperState::Paused
             },
             "r\n" => {
                 println!("recording");
-                looper.state = LooperState::Recording
             },
             "c\n" => {
                 println!("playing");
-                looper.state = LooperState::Playing
+            },
+            "w\n" => {
+                println!("up");
+                tx.send(Messages::SetThru(1.0));
+            },
+            "s\n" => {
+                println!("down");
+                tx.send(Messages::SetThru(0.0));
             },
             _ => {},
         }
