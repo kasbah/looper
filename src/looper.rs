b471874fc9f12035d9d89312d9a3119c9e9eacff
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-05-18 14:31:44 +0100

    Split out looper as file

---
+++ src/looper.rs
@@ -0,0 +1,68 @@
+extern crate jack;
+use jack::prelude as j;
+
+enum LooperState {
+    Paused,
+    Playing,
+    Recording,
+}
+
+pub struct Looper {
+    loop_  : Vec<f32>,
+    thru   : f32,
+    state  : LooperState,
+    cursor : usize,
+}
+
+impl Looper {
+    pub fn new() -> Looper {
+       Looper {
+           loop_  : Vec::new(),
+           thru   : 1.0,
+           state  : LooperState::Paused,
+           cursor : 0,
+       }
+    }
+    fn run_thru (
+        &self,
+        in_a_p  : &j::AudioInPort,
+        in_b_p  : &j::AudioInPort,
+        out_a_p : &mut j::AudioOutPort,
+        out_b_p : &mut j::AudioOutPort
+    ) {
+        let mut amplified_a : Vec<f32> = in_a_p.to_vec();
+        let mut amplified_b : Vec<f32> = in_b_p.to_vec();
+        amplified_a = amplified_a.iter().map(|&x| x * self.thru).collect();
+        amplified_b = amplified_b.iter().map(|&x| x * self.thru).collect();
+        out_a_p.clone_from_slice(&amplified_a);
+        out_b_p.clone_from_slice(&amplified_b);
+    }
+    fn advance(&mut self, n_frames: j::JackFrames) {
+        let new_cursor = self.cursor + n_frames as usize;
+        let length = self.loop_.len();
+        if new_cursor >= length {
+            self.cursor = new_cursor - length;
+        } else {
+            self.cursor = new_cursor;
+        }
+    }
+    pub fn run(
+        &mut self,
+        n_frames : j::JackFrames,
+        in_a_p   : &j::AudioInPort,
+        in_b_p   : &j::AudioInPort,
+        out_a_p  : &mut j::AudioOutPort,
+        out_b_p  : &mut j::AudioOutPort
+    ) {
+        match self.state {
+            LooperState::Paused => {
+            }
+            LooperState::Playing => {
+                self.advance(n_frames);
+            }
+            LooperState::Recording => {
+            }
+        }
+        self.run_thru(&in_a_p, &in_b_p,  out_a_p,  out_b_p);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,10 @@
 extern crate jack;
 use jack::prelude as j;
+
 use std::io;
 
-enum LooperState {
-    Paused,
-    Playing,
-    Recording,
-}
-
-struct Looper {
-    loop_: Vec<f32>,
-    thru: f32,
-    state: LooperState,
-    cursor: usize,
-}
-
-impl Looper {
-    fn new() -> Looper {
-       Looper {
-           loop_: Vec::new(),
-           thru: 1.0,
-           state: LooperState::Paused,
-           cursor: 0,
-       }
-    }
-    fn run_thru(&self, in_a_p: &j::AudioInPort, in_b_p: &j::AudioInPort, out_a_p: &mut j::AudioOutPort, out_b_p: &mut j::AudioOutPort) {
-        let mut amplified_a : Vec<f32> = in_a_p.to_vec();
-        let mut amplified_b : Vec<f32> = in_b_p.to_vec();
-        amplified_a = amplified_a.iter().map(|&x| x * self.thru).collect();
-        amplified_b = amplified_b.iter().map(|&x| x * self.thru).collect();
-        out_a_p.clone_from_slice(&amplified_a);
-        out_b_p.clone_from_slice(&amplified_b);
-    }
-    fn advance(&mut self, n_frames: j::JackFrames) {
-        let new_cursor = self.cursor + n_frames as usize;
-        let length = self.loop_.len();
-        if new_cursor >= length {
-            self.cursor = new_cursor - length;
-        } else {
-            self.cursor = new_cursor;
-        }
-    }
-    fn run(&mut self, n_frames: j::JackFrames, in_a_p: &j::AudioInPort, in_b_p: &j::AudioInPort, out_a_p: &mut j::AudioOutPort, out_b_p: &mut j::AudioOutPort) {
-        match self.state {
-            LooperState::Paused => {
-            }
-            LooperState::Playing => {
-                self.advance(n_frames);
-            }
-            LooperState::Recording => {
-            }
-        }
-        self.run_thru(&in_a_p, &in_b_p,  out_a_p,  out_b_p);
-    }
-}
+mod looper;
+use looper::*;
 
 fn main() {
     // Create client
@@ -62,16 +13,16 @@ fn main() {
 
     // Register ports. They will be used in a callback that will be
     // called when new data is available.
-    let in_a = client.register_port("rust_in_l", j::AudioInSpec::default()).unwrap();
-    let in_b = client.register_port("rust_in_r", j::AudioInSpec::default()).unwrap();
-    let mut out_a = client.register_port("rust_out_l", j::AudioOutSpec::default()).unwrap();
-    let mut out_b = client.register_port("rust_out_r", j::AudioOutSpec::default()).unwrap();
+    let in_a       = client.register_port("rust_in_l", j::AudioInSpec::default()).unwrap();
+    let in_b       = client.register_port("rust_in_r", j::AudioInSpec::default()).unwrap();
+    let mut out_a  = client.register_port("rust_out_l", j::AudioOutSpec::default()).unwrap();
+    let mut out_b  = client.register_port("rust_out_r", j::AudioOutSpec::default()).unwrap();
     let mut looper = Looper::new();
     let process_callback = move |_: &j::Client, ps: &j::ProcessScope| -> j::JackControl {
         let mut out_a_p = j::AudioOutPort::new(&mut out_a, ps);
         let mut out_b_p = j::AudioOutPort::new(&mut out_b, ps);
-        let in_a_p = j::AudioInPort::new(&in_a, ps);
-        let in_b_p = j::AudioInPort::new(&in_b, ps);
+        let in_a_p      = j::AudioInPort::new(&in_a, ps);
+        let in_b_p      = j::AudioInPort::new(&in_b, ps);
         looper.run(ps.n_frames(), &in_a_p, &in_b_p, &mut out_a_p, &mut out_b_p);
         j::JackControl::Continue
     };
